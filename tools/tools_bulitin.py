ca8c69ace470f5cde402b1c6c7a76dd427c5af37
nithin-dharavathu <nithindharavath01@gmail.com>
2026-08-08 23:01:53 +0530

    tool creation using the structured tool and pydantic

--- tools/tools_bulitin.py
+++ tools/tools_bulitin.py
@@ -1,45 +1,44 @@
-# # #search tool web
+# #search tool web
 
-# from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import DuckDuckGoSearchRun
 
-# search_tool = DuckDuckGoSearchRun()
+search_tool = DuckDuckGoSearchRun()
 
-# result = search_tool.invoke("what is the latest news today in tech?")
+result = search_tool.invoke("what is the latest news today in tech?")
 
-# print(result)
+print(result)
 
-# #shell tool _ system works cl
+#shell tool _ system works cl
 
-# from langchain_community.tools import ShellTool
+from langchain_community.tools import ShellTool
 
-# shell_tool = ShellTool()
+shell_tool = ShellTool()
 
-# system_result = shell_tool.invoke("dir")
+system_result = shell_tool.invoke("dir")
 
-# print(system_result)
+print(system_result)
 
+## custum tool using the @decarator ##
 
-#custum tool 
+from langchain_core.tools import tool
 
-# from langchain_core.tools import tool
+#step - 1 define function
+def addition(a, b):
+    """add the a and b"""
+    return (a+b)
+#step-2 add type hinting
+def addition(a:int, b:int) -> int:
+    """add the a and b"""
+    return (a+b)
+#step-3 add the tool decorator
+@tool
+def addition(a:int, b:int) -> int:
+    """add the a and b"""
+    return (a+b)
 
-# #step - 1 define function
-# def addition(a, b):
-#     """add the a and b"""
-#     return (a+b)
-# #step-2 add type hinting
-# def addition(a:int, b:int) -> int:
-#     """add the a and b"""
-#     return (a+b)
-# #step-3 add the tool decorator
-# @tool
-# def addition(a:int, b:int) -> int:
-#     """add the a and b"""
-#     return (a+b)
+result = addition.invoke({"a" : 50, "b" : 10})
 
-# result = addition.invoke({"a" : 50, "b" : 10})
-
-# print(addition.args_schema)
+print(addition.args_schema)
 
 ##method -2 :suing the structuredTool##
 from langchain_core.tools import StructuredTool
